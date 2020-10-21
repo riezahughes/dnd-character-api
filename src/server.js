2b3b142e3c444a36ccac0845cdc8d20ab5f14c79
Dan <dan@ekwholesale.co.uk>
2020-10-21 13:09:47 +0100

    Changed Yoga for Apollo

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 
-const { GraphQLServer } = require('graphql-yoga');
+const { ApolloServer, gql } = require('apollo-server');
 const resolvers = require('./includes/resolvers');
 
 const typeDefs = `
     type Query {
+        "Just a quick hello"
         hello(name: String): String!
     }
 `;
 
-const server = new GraphQLServer({ typeDefs, resolvers });
-server.start({ port: process.env.PORT });
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen({ port: process.env.PORT });
 console.log(`Server is now running on localhost:${process.env.PORT}`);
