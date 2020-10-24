1d9423ea152699f462009ab70e456bffe6ea413e
DESKTOP-VN1U2BM\Dan <dan@ekwholesale.co.uk>
2020-10-24 12:58:25 +0100

    Updates for CRUD

--- src/includes/resolvers.js
+++ src/includes/resolvers.js
@@ -1,14 +1,53 @@
 const resolvers = {
   Query: {
+    // singles
+    character: (_, args, context) => context.prisma.character.findOne({ where: { id: args.id } }),
+    race: (_, args, context) => context.prisma.race.findOne({ where: { id: args.id } }),
+    class: (_, args, context) => context.prisma.class.findOne({ where: { id: args.id } }),
+    dataresource: (_, args, context) => context.prisma.dataresource.findOne({ where: { id: args.id } }),
+    proficiency: (_, args, context) => context.prisma.proficiency.findOne({ where: { id: args.id } }),
+    equipmentsingle: (_, args, context) => context.prisma.equipment.findOne({ where: { id: args.id } }),
+    skill: (_, args, context) => context.prisma.skill.findOne({ where: { id: args.id } }),
+    condition: (_, args, context) => context.prisma.condition.findOne({ where: { id: args.id } }),
+    background: (_, args, context) => context.prisma.background.findOne({ where: { id: args.id } }),
+    language: (_, args, context) => context.prisma.language.findOne({ where: { id: args.id } }),
+    feat: (_, args, context) => context.prisma.feat.findOne({ where: { id: args.id } }),
+    spell: (_, args, context) => context.prisma.spell.findOne({ where: { id: args.id } }),
+    spellschool: (_, args, context) => context.prisma.spellschool.findOne({ where: { id: args.id } }),
+    psyonic: (_, args, context) => context.prisma.psyonic.findOne({ where: { id: args.id } }),
+    deity: (_, args, context) => context.prisma.deity.findOne({ where: { id: args.id } }),
+    supernaturalpassive: (_, args, context) => context.prisma.supernaturalpassive.findOne({ where: { id: args.id } }),
+    vehicle: (_, args, context) => context.prisma.vehicle.findOne({ where: { id: args.id } }),
+
+    // manys
     characters: (_, args, context) => context.prisma.character.findMany(),
     races: (_, args, context) => context.prisma.race.findMany(),
     classes: (_, args, context) => context.prisma.class.findMany(),
-    dataResources: (_, args, context) => context.prisma.dataResource.findMany(),
+    dataresources: (_, args, context) => context.prisma.dataResource.findMany(),
+    proficiencies: (_, args, context) => context.prisma.proficiency.findMany(),
+    equipmentmany: (_, args, context) => context.prisma.equipment.findMany(),
+    skills: (_, args, context) => context.prisma.skill.findMany(),
+    conditions: (_, args, context) => context.prisma.condition.findMany(),
+    backgrounds: (_, args, context) => context.prisma.background.findMany(),
+    languages: (_, args, context) => context.prisma.language.findMany(),
+    feats: (_, args, context) => context.prisma.feat.findMany(),
+    spells: (_, args, context) => context.prisma.spell.findMany(),
+    spellschools: (_, args, context) => context.prisma.spellschool.findMany(),
+    psyonics: (_, args, context) => context.prisma.psyonic.findMany(),
+    deities: (_, args, context) => context.prisma.deity.findMany(),
+    supernaturalpassives: (_, args, context) => context.prisma.supernaturalpassive.findMany(),
+    vehicles: (_, args, context) => context.prisma.vehicle.findMany(),
   },
   Mutation: {
     createRace: (_, args, context) => context.prisma.race.create({
       data: {
         name: args.name,
+      },
+    }),
+    updateRace: (_, args, context) => context.prisma.race.update({
+      data: {
+        name: args.name,
+        traits: args.traits,
       },
     }),
     deleteRace: (_, args, context) => context.prisma.race.delete({
